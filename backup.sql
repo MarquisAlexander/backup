b225004233e58099d8aaa3ed7dee538a938eed15
root <root@srv.kiteradar.com.br>
2021-01-21 15:34:10 +0000

    09

---
+++ backup.sql
@@ -0,0 +1,1 @@
+OCI runtime exec failed: exec failed: container_linux.go:370: starting container process caused: exec: "/usr/bin/postgres": stat /usr/bin/postgres: no such file or directory: unknown
