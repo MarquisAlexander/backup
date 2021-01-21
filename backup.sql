c10d47cb800ab7ef47404c773aa32bde81a259d9
root <root@srv.kiteradar.com.br>
2021-01-21 17:04:41 +0000

    remove trash

--- backup.sql
+++
@@ -1,1 +0,0 @@
-OCI runtime exec failed: exec failed: container_linux.go:370: starting container process caused: exec: "/usr/bin/postgres": stat /usr/bin/postgres: no such file or directory: unknown
--- backupDB.sql
+++
@@ -1,1 +0,0 @@
-OCI runtime exec failed: exec failed: container_linux.go:370: starting container process caused: exec: "-c": executable file not found in $PATH: unknown
